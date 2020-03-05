99add521b6d8c8203f76f245bc4bf649a491db86
Negin Sauermann <negin.sauermann@campbrain.com>
2020-03-05 15:23:31 -0500

    ts compiled to es6 ☺

--- examples/hello-ts/src/index.js
+++ examples/hello-ts/src/index.js
@@ -2,31 +2,20 @@
  * Create a promise that resolves after some time
  * @param n number of milliseconds before promise resolves
  */
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 //resolves over a set period of time, pause exec
 function timeout(n) {
-    return new Promise(res => setTimeout(res, n));
+  return new Promise(res => setTimeout(res, n));
 }
 /**
  * Add three numbers
  * @param a first number
  * @param b second
  */
-export function addNumbers(a, b) {
-    return __awaiter(this, void 0, void 0, function* () {
-        yield timeout(500);
-        return a + b;
-    });
+export async function addNumbers(a, b) {
+  await timeout(500);
+  return a + b;
 }
 //== Run the program ==//
-(() => __awaiter(void 0, void 0, void 0, function* () {
-    console.log(yield addNumbers(3, 4));
-}))();
+(async () => {
+  console.log(await addNumbers(3, 4));
+})();
